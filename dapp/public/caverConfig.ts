1b458081faa53f3c00830e34565ed3e489e0acb3
choiseongjun <csj2702@naver.com>
2022-05-30 10:48:53 +0900

    next setting

---
+++ dapp/public/caverConfig.ts
@@ -0,0 +1,9 @@
+import {AbiItem} from "caver-js";
+
+export const MINT_GEM_TOKEN_ADDRESS=""
+
+export const SALE_GEM_TOKEN_ADDRESS=""
+
+export const MINT_GEM_TOKEN_ABI:AbiItem[]=[]
+
+export const SALE_GEM_TOKEN_ABI:AbiItem[] = []
